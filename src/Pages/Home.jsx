e19f85eae87490b66de2a333ab65dfd5d2fcd44c
paramesh22 <parameshkumar22@gmail.com>
2023-11-13 16:04:17 +0530

    added input for searching and using fetch for data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,25 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 const Home = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  function searchChangeHandler(event) {
+    setSearchInput(event.target.value);
+  }
+  async function submitHandler(event) {
+    event.preventDefault();
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchInput}`
+    );
+    const body = await response.json();
+    console.log(body);
+  }
   return (
     <div>
-      <Link to="/starred">Go to starred page</Link>
+      <form action="submit" onClick={submitHandler}>
+        <input type="text" onChange={searchChangeHandler}></input>
+        <button type="submit">search</button>
+      </form>
     </div>
   );
 };
