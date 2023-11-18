0c32e44c1bc69747b4de4a22953a249f5ac0959b
paramesh22 <parameshkumar22@gmail.com>
2023-11-18 21:13:12 +0530

    added search for actors and also changed onclick to onsubmit for form which made somuch error

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,15 @@
-import { searchForShows } from "../api/Tvmaze";
+import { searchForPeople, searchForShows } from "../api/Tvmaze";
 import { useState } from "react";
 
 const Home = () => {
+  const [searchOption, setsearchOption] = useState("shows");
   const [searchInput, setSearchInput] = useState("");
-  const [api, setApi] = useState([]);
+  const [api, setApi] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
+  function radiochange(event) {
+    setsearchOption(event.target.value);
+  }
+  console.log(searchOption);
   function searchChangeHandler(event) {
     setSearchInput(event.target.value);
   }
@@ -12,23 +17,53 @@ const Home = () => {
     event.preventDefault();
     try {
       setApiDataError(null);
-      const result = await searchForShows(searchInput);
-      setApi(result);
+      if (searchOption === "shows") {
+        const result = await searchForShows(searchInput);
+        setApi(result);
+      } else {
+        const result = await searchForPeople(searchInput);
+        setApi(result);
+      }
     } catch (error) {
       setApiDataError(error);
     }
-    console.log(apiDataError);
   }
+
   const renderApiData = () => {
     if (apiDataError) return <div>Error occured : {apiDataError.message}</div>;
     if (api) {
-      return api.map((item) => <div id={item.show.id}>{item.show.name}</div>);
+      return api[0].show
+        ? api.map((item) => <div id={item.show.id}>{item.show.name}</div>)
+        : api.map((item) => <div id={item.person.id}>{item.person.name}</div>);
     }
+
+    return null;
   };
   return (
     <div>
-      <form action="submit" onClick={submitHandler}>
-        <input type="text" onChange={searchChangeHandler}></input>
+      <form action="submit" onSubmit={submitHandler}>
+        <input type="text" onChange={searchChangeHandler} />
+        <label>
+          Shows
+          <input
+            checked={searchOption === "shows"}
+            type="radio"
+            name="search-option"
+            value="shows"
+            onClick={radiochange}
+          />
+        </label>
+        <label>
+          Actors
+          <input
+            checked={searchOption === "actors"}
+            type="radio"
+            name="search-option"
+            value="actors"
+            onClick={radiochange}
+          />
+        </label>
+
         <button type="submit">search</button>
       </form>
       <div>{renderApiData()}</div>
