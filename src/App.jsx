8e547a8f584e6dd7d727c5c3f4c084993ece3fa8
paramesh22 <parameshkumar22@gmail.com>
2023-11-20 23:33:19 +0530

    installed react query and used it inplace of getDataById custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./Pages/Home";
 import Starred from "./Pages/Starred";
 import Show from "./Pages/Show";
+const queryClient = new QueryClient();
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/starred" element={<Starred />}></Route>
-        </Route>
-        {/* This is dynamic routing */}
-        <Route path="/show/:showId" element={<Show />}></Route>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/starred" element={<Starred />}></Route>
+          </Route>
+          {/* This is dynamic routing */}
+          <Route path="/show/:showId" element={<Show />}></Route>
 
-        <Route path="*" element={<div>Not found</div>}></Route>
-        {/* <Route path="/" element={<App />}>
+          <Route path="*" element={<div>Not found</div>}></Route>
+          {/* <Route path="/" element={<App />}>
           <Route index element={<Home />} />
           <Route path="teams" element={<Teams />}>
             <Route path=":teamId" element={<Team />} />
@@ -28,8 +31,9 @@ function App() {
           <Route path="/tos" element={<Tos />} />
         </Route>
         <Route path="contact-us" element={<Contact />} /> */}
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -1,26 +1,15 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getShowById } from "../api/Tvmaze";
-const useShowById = (showId) => {
-  const [showData, setShowData] = useState(null);
-  const [showError, setShowError] = useState(null);
+import { useQuery } from "@tanstack/react-query";
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        setShowData(data);
-      } catch (error) {
-        setShowError(error);
-      }
-    }
-    fetchData();
-  }, [showId]);
-  return { showData, showError };
-};
 const Show = () => {
   const { showId } = useParams();
-  const { showData, showError } = useShowById(showId);
+
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ["show", showId],
+    queryFn: () => getShowById(showId),
+  });
+
   if (showError) return <div>We have an error showError.message</div>;
   if (showData) return <div>Data for this : {showData.name}</div>;
   return <div>Data is loading</div>;
